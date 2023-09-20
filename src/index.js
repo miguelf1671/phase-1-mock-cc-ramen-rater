6893fc8f81ea2255d382236669b1c704bcfc9376
miguel flores ramirez <miguelf1671@gmail.com>
2023-09-20 17:04:55 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -2,10 +2,30 @@
  const ramens = "http://localhost:3000/ramens";
  const ramen_detail = document.querySelector("#ramen-detail");
  let ramenMenu_div = document.querySelector("#ramen-menu");
+ let detail_img = document.querySelector(".detail-image");
+ 
+ let imgName = document.querySelector(".name");
+ let restaurant = document.querySelector(".restaurant");
+ let rating = document.querySelector("#rating-display");
+ let comment = document.querySelector("#comment-display");
+ 
+ function handleImageClick(ramen){
+    detail_img.src = ramen.image;
+    imgName.textContent = ramen.name;
+    restaurant.textContent = ramen.restaurant;
+    rating.textContent = ramen.rating;
+    comment.textContent = ramen.comment;
+};
 
  fetch(ramens)
  .then(response => response.json())
  .then(ramens => {
+    detail_img.src = ramens[0].image;
+    imgName.textContent=ramens[0].name;
+    restaurant.textContent = ramens[0].restaurant;
+    rating.textContent = ramens[0].rating;
+    comment.textContent = ramens[0].comment;
+
     ramens.forEach(ramen => {
         getRamen(ramen, ramens);
     });
@@ -17,26 +37,17 @@
     ramen_img_ele.src = ramen.image;
     ramenMenu_div.append(ramen_img_ele);
 
-    ramen_img_ele.addEventListener('click',() => {
-        // let detail_img = document.querySelector("#big-img");
-        // detail_img.src = ramen.image;
-
-        let imgName = document.querySelector(".name");
-        imgName.textContent = ramen.name;
-    
-        let restaurant = document.querySelector(".restaurant");
-        restaurant.textContent = ramen.restaurant;
-
-        let rating = document.querySelector("#rating-display");
-        rating.textContent = ramen.rating;
-
-        let comment = document.querySelector("#comment-display");
-        comment.textContent = ramen.comment;
+    ramen_img_ele.addEventListener('click',()=>{
+        handleImageClick(ramen);
+        
     });
+}
 
- }
+
+ 
  let newForm = document.querySelector("#new-ramen");
- newForm.addEventListener('submit',() => {
+ newForm.addEventListener('submit',(event) => {
+    event.preventDefault();
     let new_img = document.createElement("img");
     let new_img_submit = document.querySelector("#new-image").value;
     new_img.src = new_img_submit
@@ -44,14 +55,27 @@
 
     let newName = document.querySelector("#new-name").value;
     let newRestaurant = document.querySelector("#new-restaurant").value;
-    let newRating = document.querySelector("#new-rating");
-    let newcomment = document.querySelector("#new-comment");
+    let newRating = document.querySelector("#new-rating").value;
+    let newcomment = document.querySelector("#new-comment").value;
 
     ramenMenu_div.append(new_img);
+
+    new_img.addEventListener('click', ()=>{
+        handleImageClick({
+            image:new_img_submit,
+            name: newName,
+            restaurant: newRestaurant,
+            rating: newRating,
+            comment: newcomment
+
+
+        });
+    });
     
 
 
 
  });
  console.log(ramenMenu_div);
+ console.log(ramens[-1]);
 
